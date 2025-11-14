725228932e331ef34849c53fc52923733c85bbc3
Fideliog <eduardoledo36@gmail.com>
2025-11-14 04:21:02 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: -23.55052,
+            longitude: -46.633308,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{ latitude: -23.55052, longitude: -46.633308 }}
+            title={"São Paulo"}
+            description={"Capital financeira do Brasil"}
+          />
+        </MapView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +30,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
